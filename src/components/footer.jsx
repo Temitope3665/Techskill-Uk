2637a3af7dbf0313681c55e333d13e8a23570516
Your Other Name <your-email-account2@example.com>
2024-01-18 14:15:56 +0100

    update spaces in footer component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,30 +3,30 @@ import { footerData } from '@/config/dataa';
 
 const Footer = () => {
   return (
-    <div className="bg-yellow p-6 md:p-12">
+    <div className="bg-yellow px-6 lg:px-12 py-6">
       <img src={Logo} alt="logo" width={100} />
 
-      <div className="grid grid-cols-2 md:grid-cols-3 text-[#1D1E20] mt-16 gap-x-8 md:gap-x-16">
+      <div className="grid grid-cols-2 md:grid-cols-3 text-[#1D1E20] mt-8 gap-x-8 md:gap-x-16 text-xs lg:text-sm">
         <div>
-          <p className="font-gilroyBold mb-4">Company</p>
+          <p className="font-gilroyBold mb-2">Company</p>
           {footerData.company.map((com) => (
-            <p key={com.title} className="py-2 md:py-3 font-gilroyMd">
+            <p key={com.title} className="py-2 font-gilroyMd">
               {com.title}
             </p>
           ))}
         </div>
         <div>
-          <p className="font-gilroyBold mb-4">Our Courses</p>
+          <p className="font-gilroyBold mb-2">Our Courses</p>
           {footerData.courses.map((com) => (
-            <p key={com.title} className="py-2 md:py-3 font-gilroyMd">
+            <p key={com.title} className="py-2 font-gilroyMd">
               {com.title}
             </p>
           ))}
         </div>
         <div>
-          <p className="font-gilroyBold mb-4">Our Contact</p>
+          <p className="font-gilroyBold mb-2">Our Contact</p>
           {footerData.contact.map((com) => (
-            <p key={com.title} className="py-2 md:py-3 font-gilroyMd">
+            <p key={com.title} className="py-2 font-gilroyMd">
               {com.title}
             </p>
           ))}
