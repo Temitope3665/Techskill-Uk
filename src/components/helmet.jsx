f033e0939c09d268f7b4a6bfd1bbaa47d6a5dc3a
Your Other Name <your-email-account2@example.com>
2024-03-05 09:47:57 +0100

    logo icon

--- src/components/helmet.jsx
+++ src/components/helmet.jsx
@@ -5,7 +5,7 @@ const ReactHelment = ({ title }) => (
     <meta charSet="utf-8" />
     <title>{title || 'Techskill UK'}</title>
     <meta name='description' content='Accelerate you career today with Digital Transformation' />
-    <link rel="icon" type="image/svg+xml" href="/logo-icon.svg" />
+    <link rel="icon" type="image/svg+xml" href="https://res.cloudinary.com/dboqyj4bp/image/upload/v1709628383/logo-icon_tgxapk.png" />
 </Helmet>
 )
 
